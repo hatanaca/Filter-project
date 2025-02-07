31c1f3f8ab26a7e1d1769ac5e81a69ac2a2e671d
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2025-02-07 11:21:04 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Arquivo Javascript carregado com sucesso!')
+document.getElementById('search').addEventListener('input', function(){
+	let filter = this.value.toLowerCase();
+	let items = document.querySelectorAll('#list li');
+
+	items.forEach(function(item) {
+		let text = item.textContent.toLowerCase();
+		if (text.includes(filter)) {
+			item.style.display = '';
+		} else {
+			item.style.display = 'none';
+		}
+	});
+});
+
